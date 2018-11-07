f2bbcb19848ba1c3bd3905bb3a3759cb2ffa4b29
Vanessa~Roux <vanessa.roux891@orange.fr>
2018-11-08 00:00:58 +0100

    ajout formulaire de creation d'un  post + affichage du nouveau post dans la categorie aide moi

--- alumnis/src/Controller/AlumnisController.php
+++ alumnis/src/Controller/AlumnisController.php
@@ -5,6 +5,10 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Posts;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\DBAL\Types\TextType;
+use Doctrine\DBAL\Types\DateType;
 
 class AlumnisController extends AbstractController
 {
@@ -27,14 +31,52 @@ class AlumnisController extends AbstractController
     /**
      * @Route("/", name="home")
      */
+//Page accueil
     public function home()
     {
         return $this->render('alumnis/home.html.twig');
     }
 
+
+    /**
+     * @Route("/alumnis/new", name="alumnis_create")
+     */
+//Page pour créer un post (avec un form objet de symfony)
+    public function create(Request $request, ObjectManager $manager)
+    {
+        $article = new Posts();
+
+        $form = $this->createFormBuilder($article)
+                        ->add('titre')
+                        ->add('contenu')
+                        ->add('date')
+                        ->getForm();
+
+        $form->handleRequest($request);
+
+        //dump($article);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            //$article->setCreatedAt(new \DateTime());
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('help_show', [
+                'id' => $article->getId()
+            ]);
+        }
+
+
+        return $this->render('alumnis/create.html.twig',[
+            'formPost' => $form->createView() //affichage de l'objet form
+        ]);
+    }
+
+
     /**
      * @Route("/alumnis/{id}", name="help_show")
      */
+//Page montrer un post
     public function show($id)
     {
         $repo = $this->getDoctrine()->getRepository(Posts::class);
@@ -45,4 +87,5 @@ class AlumnisController extends AbstractController
             'post' => $post
         ]);
     }
+
 }
